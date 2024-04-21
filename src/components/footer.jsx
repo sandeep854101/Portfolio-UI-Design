f4aa98e4bd2479acf51924e5887030a0aa681f8d
SANDEEP KUMAR <139211042+sandeep854101@users.noreply.github.com>
2024-04-21 14:59:18 +0530

    footer link add

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from 'react-icons/fa'
+import { FaFacebook, FaInstagram, FaYoutube, FaLinkedin } from 'react-icons/fa'
 function Footer() {
     return (
         <>
@@ -8,10 +8,10 @@ function Footer() {
                 <div className='max-w-screen-2x1 container mx-auto px-4 md:px-20 '>
                     <div className='flex flex-col items-center justify-center'>
                         <div className='flex space-x-4'>
-                            <FaFacebook size={24}/>
-                            <FaInstagram size={24}/>
-                            <FaLinkedin size={24}/>
-                            <FaTwitter size={24}/>
+                            <a href="https://www.facebook.com/profile.php?id=100056538092127" target="_blank"><FaFacebook className='text-xl md:text-3xl hover:scale-110 duration-200  cursor-pointer'/></a>
+                            <a href="https://www.instagram.com/sandeep854101/" target="_blank"><FaInstagram className='text-xl md:text-3xl hover:scale-110 duration-200  cursor-pointer'/></a>
+                            <a href="https://www.linkedin.com/in/sandeep-kumar-031782284/" target="_blank"><FaLinkedin className='text-xl md:text-3xl hover:scale-110 duration-200  cursor-pointer'/></a>
+                            <a href="https://www.youtube.com/channel/UCmkh9CqlvV4ds-vLNgUZLnA" target='-blank'><FaYoutube className='text-xl md:text-3xl hover:scale-110 duration-200  cursor-pointer' /></a>
                         </div>
                         <div className='mt-8 border-t border-black-700 pt-8 flex flex-col items-center'>
                             <p className='text-sm '>&copy; 2024 Your Company. All rights reversed.  </p>
