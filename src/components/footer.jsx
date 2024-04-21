998fa8080f1b4542a4967dd763d97bee33b92598
SANDEEP KUMAR <139211042+sandeep854101@users.noreply.github.com>
2024-04-21 15:33:44 +0530

    footer icon changeing test

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
                             <a href="https://www.facebook.com/profile.php?id=100056538092127" target="_blank"><FaFacebook className='text-xl md:text-3xl hover:scale-110 duration-200  cursor-pointer' md:size={50}/></a>
                             <a href="https://www.instagram.com/sandeep854101/" target="_blank"><FaInstagram className='text-xl md:text-3xl hover:scale-110 duration-200  cursor-pointer' md:size={50}/></a>
                             <a href="https://www.linkedin.com/in/sandeep-kumar-031782284/" target="_blank"><FaLinkedin className='text-xl md:text-3xl hover:scale-110 duration-200  cursor-pointer' md:size={50}/></a>
-                            <a href="https://www.youtube.com/channel/UCmkh9CqlvV4ds-vLNgUZLnA" target='-blank'><FaYoutube className='text-xl md:text-3xl hover:scale-110 duration-200  cursor-pointer' md:size={50}/></a>
+                            <a href="https://www.youtube.com/channel/UCmkh9CqlvV4ds-vLNgUZLnA" target='-blank'><FaYoutube className='text-xl md:text-3xl hover:scale-110 duration-200  cursor-pointer' sm:size={50}/></a>
                         </div>
                         <div className='mt-8 border-t border-black-700 pt-8 flex flex-col items-center'>
                             <p className='text-sm '>&copy; 2024 Your Company. All rights reversed.  </p>
