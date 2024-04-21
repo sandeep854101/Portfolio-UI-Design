6a104b0267d6f48d378d413587f26b6deb0b65d3
SANDEEP KUMAR <139211042+sandeep854101@users.noreply.github.com>
2024-04-21 15:31:47 +0530

    footer icon changeing test

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -52,7 +52,7 @@ function Experience() {
         <div className='grid grid-cols-2 md:grid-cols-5 gap-7 my-16'>
             {
                 cardItem.map(({id,logo,name})=>(
-                    <div className='flex flex-col items-center justify-center border-[2px] rounded-full shadow-md md:w-[200px] md:h-[200px] p-1 cursor-pointer hover:scale-110 duration-300' key={id}>
+                    <div className='flex flex-col items-center justify-center border-[2px] rounded-full shadow-md md:w-[200px] md:h-[200px] p-1 cursor-pointer lg:hover:scale-105 duration-300' key={id}>
                         <img src={logo} className='w-[150px]
                          rounded-full 
                           ' alt="" />
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,10 +8,10 @@ function Footer() {
                 <div className='max-w-screen-2x1 container mx-auto px-4 md:px-20 '>
                     <div className='flex flex-col items-center justify-center'>
                         <div className='flex space-x-4'>
-                            <a href="https://www.facebook.com/profile.php?id=100056538092127" target="_blank"><FaFacebook className='text-xl md:text-3xl hover:scale-110 duration-200  cursor-pointer'/></a>
-                            <a href="https://www.instagram.com/sandeep854101/" target="_blank"><FaInstagram className='text-xl md:text-3xl hover:scale-110 duration-200  cursor-pointer'/></a>
-                            <a href="https://www.linkedin.com/in/sandeep-kumar-031782284/" target="_blank"><FaLinkedin className='text-xl md:text-3xl hover:scale-110 duration-200  cursor-pointer'/></a>
-                            <a href="https://www.youtube.com/channel/UCmkh9CqlvV4ds-vLNgUZLnA" target='-blank'><FaYoutube className='text-xl md:text-3xl hover:scale-110 duration-200  cursor-pointer' /></a>
+                            <a href="https://www.facebook.com/profile.php?id=100056538092127" target="_blank"><FaFacebook className='text-xl md:text-3xl hover:scale-110 duration-200  cursor-pointer' md:size={50}/></a>
+                            <a href="https://www.instagram.com/sandeep854101/" target="_blank"><FaInstagram className='text-xl md:text-3xl hover:scale-110 duration-200  cursor-pointer' md:size={50}/></a>
+                            <a href="https://www.linkedin.com/in/sandeep-kumar-031782284/" target="_blank"><FaLinkedin className='text-xl md:text-3xl hover:scale-110 duration-200  cursor-pointer' md:size={50}/></a>
+                            <a href="https://www.youtube.com/channel/UCmkh9CqlvV4ds-vLNgUZLnA" target='-blank'><FaYoutube className='text-xl md:text-3xl hover:scale-110 duration-200  cursor-pointer' md:size={50}/></a>
                         </div>
                         <div className='mt-8 border-t border-black-700 pt-8 flex flex-col items-center'>
                             <p className='text-sm '>&copy; 2024 Your Company. All rights reversed.  </p>
